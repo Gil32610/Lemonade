5e96f67cd9bda452c8534f862b7c30e2b83523af
GIL32610 <gil.00000032610@unicap.br>
2023-11-29 11:13:50 -0300

    Button added

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -61,7 +70,8 @@ fun LemonApp() {
                 LemonadeDisplay(
                     imageResource = painterResource(R.drawable.lemon_tree) ,
                     title = stringResource(R.string.lemon_tree_title),
-                    description = stringResource(R.string.lemon_tree_title) , modifier = Modifier.clickable { lemonadeStep =2})
+                    description = stringResource(R.string.lemon_tree_title) ,
+                    onImageClick =  { lemonadeStep =2})
             }
 
             2 -> {
@@ -71,7 +81,7 @@ fun LemonApp() {
                     imageResource = painterResource(R.drawable.lemon_squeeze),
                     title = stringResource(R.string.lemon_tap_title),
                     description = stringResource(R.string.lemon_description),
-                    modifier = Modifier.clickable {
+                    onImageClick =  {
                         count++
                         if (count == taps) {
                             lemonadeStep = 3
@@ -85,7 +95,10 @@ fun LemonApp() {
                     imageResource = painterResource(R.drawable.lemon_drink) ,
                     title = stringResource(R.string.lemonade_glass_title),
                     description = stringResource(R.string.glass_of_lemonade_description),
-                    modifier = Modifier.clickable { lemonadeStep = 4 })
+                    onImageClick = {
+                        lemonadeStep = 4
+                    }
+                )
             }
 
             4 -> {
@@ -93,9 +106,10 @@ fun LemonApp() {
                     imageResource = painterResource(R.drawable.lemon_restart) ,
                     title = stringResource(R.string.empty_glass_title),
                     description = stringResource(R.string.empty_glass_description),
-                    modifier = Modifier
-                        .clickable { lemonadeStep = 1 }
-                        )
+                    onImageClick = {
+                        lemonadeStep = 1
+                    }
+                )
             }
 
         }
@@ -103,37 +117,56 @@ fun LemonApp() {
 }
 
 @Composable
-fun LemonadeDisplay(modifier: Modifier = Modifier, imageResource: Painter, title: String, description: String){
+fun LemonadeDisplay(
+    modifier: Modifier = Modifier,
+    imageResource: Painter,
+    title: String,
+    description: String,
+    onImageClick: () -> Unit){
 
+    Row(
+    verticalAlignment = Alignment.Top,
+    horizontalArrangement = Arrangement.Center,
+    ){
     Text(
         text = stringResource(R.string.app_name),
-        modifier = modifier
-            .padding(top =18.dp,bottom = 18.dp),
+        modifier = Modifier,
         fontSize = 32.sp,
         textAlign = TextAlign.Center
     )
+}
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.wrapContentSize()
+        modifier = modifier
 
 
     ) {
+        Box(modifier = modifier){
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+                ){
+                Image(
+                    painter =imageResource,
+                    contentDescription = description,
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .padding(16.dp)
+
+                )
+            }
 
 
+        }
+
+        Spacer(modifier = modifier.height(32.dp))
         Text(
             text = title,
             fontSize = 18.sp
-        )
-        Spacer(modifier = modifier.height(32.dp))
-        Image(
-            painter =imageResource,
-            contentDescription = description,
-            modifier = Modifier
-                .background(Color.Cyan)
-                .wrapContentSize()
-
-
         )
 
     }
