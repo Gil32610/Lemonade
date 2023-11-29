088169529e6f527584b03edf0d90350067089c19
GIL32610 <gil.00000032610@unicap.br>
2023-11-28 23:46:56 -0300

    Layout almost ready

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,10 +25,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,7 +93,9 @@ fun LemonApp() {
                     imageResource = painterResource(R.drawable.lemon_restart) ,
                     title = stringResource(R.string.empty_glass_title),
                     description = stringResource(R.string.empty_glass_description),
-                    modifier = Modifier.clickable { lemonadeStep = 1 })
+                    modifier = Modifier
+                        .clickable { lemonadeStep = 1 }
+                        )
             }
 
         }
@@ -97,11 +104,23 @@ fun LemonApp() {
 
 @Composable
 fun LemonadeDisplay(modifier: Modifier = Modifier, imageResource: Painter, title: String, description: String){
+
+    Text(
+        text = stringResource(R.string.app_name),
+        modifier = modifier
+            .padding(top =18.dp,bottom = 18.dp),
+        fontSize = 32.sp,
+        textAlign = TextAlign.Center
+    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.wrapContentSize()
+
+
     ) {
+
+
         Text(
             text = title,
             fontSize = 18.sp
@@ -111,8 +130,9 @@ fun LemonadeDisplay(modifier: Modifier = Modifier, imageResource: Painter, title
             painter =imageResource,
             contentDescription = description,
             modifier = Modifier
-                .background(Color.Yellow)
+                .background(Color.Cyan)
                 .wrapContentSize()
+
 
         )
 
